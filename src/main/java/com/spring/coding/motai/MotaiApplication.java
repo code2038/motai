cad99d6c68d5b7c25e573ed53d34edfd792c7423
MoTai <zhuye557@126.com>
2020-04-23 04:39:44 +0800

    'init'

---
+++ src/main/java/com/spring/coding/motai/MotaiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.coding.motai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotaiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MotaiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/spring/coding/motai/MotaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.coding.motai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotaiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
